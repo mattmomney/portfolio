dd13248309d98a9e9cc7ebf14dbfa4f7495569a2
Erin <mattmomney@hotmail.com>
2023-09-12 01:27:00 -0400

    creates intro component, adds headshot image

---
+++ components/intro.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+import headshot from "@/public/headshot.jpg";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <Image
+            src={headshot}
+            alt="Portrait of Matthew Momney"
+            width="192"
+            height="192"
+            quality="95"
+            priority={true}
+            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+          />
+          <span className="absolute bottom-0 right-0 text-4xl">👋</span>
+        </div>
+      </div>
+    </section>
+  );
+}
