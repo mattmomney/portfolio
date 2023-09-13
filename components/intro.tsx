bb47c64579af69bf9218840eae2dcfdbb58c3e9a
Erin <mattmomney@hotmail.com>
2023-09-13 02:22:36 -0400

    adds animation styles to header links, adds custom hook to check if section is in view

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import headshot from "@/public/headshot.jpg";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
   return (
     <section
+      ref={ref}
       id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem] scroll-mt-28"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
       <div className="flex items-center justify-center">
         <div className="relative">
