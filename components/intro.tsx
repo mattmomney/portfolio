77d17abd54811b539958c4579ee5919410271d1b
Erin <mattmomney@hotmail.com>
2023-09-13 02:24:23 -0400

    removes unused import

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React from "react";
 import headshot from "@/public/headshot.jpg";
 import { motion } from "framer-motion";
 import Link from "next/link";
