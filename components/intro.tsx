7dcc23948afc7a1c74584898f35a291aa21695b9
Erin <mattmomney@hotmail.com>
2023-09-14 01:58:12 -0400

    testing deployments

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,7 +62,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Matthew.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Matthew Momney.</span> I'm a{" "}
         <span className="font-bold">Software developer</span> with{" "}
         <span className="font-bold">10 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My new focus is{" "}
